e41771cfbfce546dcd21200e51b6fef8562f4bcb
tsumo <tsumo@users.noreply.github.com>
2020-12-15 11:26:00 +0300

    feat: resize container grab element

--- src/components/ResizeContainer/ResizeContainer.tsx
+++ src/components/ResizeContainer/ResizeContainer.tsx
@@ -2,5 +2,10 @@ import { FC } from 'react';
 import s from './ResizeContainer.module.scss';
 
 export const ResizeContainer: FC = ({ children }) => {
-  return <div className={s.root}>{children}</div>;
+  return (
+    <div className={s.root}>
+      {children}
+      <div className={s.corner} />
+    </div>
+  );
 };
